d97898d65a90ebc05e5cc6b6960320ef77c3ca47
NahedIssaoui <issaouinahed96@gmail.com>
2023-10-22 10:39:44 +0100

    front 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/header/Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,6 +8,12 @@ export default function Header() {
         src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/30c0b38d-85b7-4d15-85de-1c57bd48c0f5/deql6ci-c49bf1a3-e984-4570-9842-d32669e95468.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzMwYzBiMzhkLTg1YjctNGQxNS04NWRlLTFjNTdiZDQ4YzBmNVwvZGVxbDZjaS1jNDliZjFhMy1lOTg0LTQ1NzAtOTg0Mi1kMzI2NjllOTU0NjguZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.24Q-Omcq6YpjA3hfdUFAnw3qPmufgpoAQCBtAeuvRBo"
         alt=""
       />
+      {/* <video className="headerImg" 
+      src="https://www.youtube.com/embed/8nKJCNgiVhc?si=qUE0XlJhDZlD_Pfn&start=12" 
+      controls="controls" 
+      autoplay="true"
+      muted="true" loop="true"
+      /> */}
     </div>
   );
 }
